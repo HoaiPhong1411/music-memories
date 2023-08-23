33058e5538b2e204610a80f6bcb1436b4715b91c
Phong <96569661+HoaiPhong1411@users.noreply.github.com>
2023-08-23 22:05:24 +0700

    fix ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
     return (
         <>
             <AudioProvider>
-                <div className="bg-home-mobile md:bg-home w-full h-screen flex justify-center items-center bg-contain">
+                <div className="bg-home-mobile md:bg-home w-full h-screen flex justify-center items-center bg-cover">
                     <Box classN="w-full md:w-3/4 p-4 md:p-6 flex flex-col-reverse md:flex-row gap-4 md:gap-6 mx-4">
                         <BoxListAudio audios={audios} />
                         <BoxAudio />
