501e4afb7c0a531599307fa72303f372e3ffee54
Phong <96569661+HoaiPhong1411@users.noreply.github.com>
2023-08-25 19:34:57 +0700

    fix scroll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body className={inter.className}>
+            <body
+                className={`${inter.className} h-screen bg-home-mobile md:bg-home bg-cover overflow-hidden flex justify-center items-center`}
+            >
                 <ReduxProvider> {children}</ReduxProvider>
             </body>
         </html>
